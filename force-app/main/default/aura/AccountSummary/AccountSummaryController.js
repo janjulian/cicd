8525a297ecac35f79b725ec8c13e1fee75bc2468
Jan Julian <jan@graycloud.ca>
2019-08-09 15:51:17 -0700

    Added list of aliases on account summarh.

--- force-app/main/default/aura/AccountSummary/AccountSummaryController.js
+++ force-app/main/default/aura/AccountSummary/AccountSummaryController.js
@@ -1,5 +1,19 @@
 ({
-    myAction : function(component, event, helper) {
+    init : function(component, event, helper) {
+        var action = component.get("c.getAliases");
+        
+        action.setParams({
+            accountId : component.get("v.recordId")
+        });
+        
+        action.setCallback(this, function(response) {
+            var state = response.getState();
 
+            if (state === "SUCCESS") {
+                component.set("v.aliases", response.getReturnValue());
+            }
+        });
+
+        $A.enqueueAction(action);
     }
 })
